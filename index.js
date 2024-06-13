91e1a9f35b7c79d09ef658cdcb9695dffea02734
pavanudavant825 <pavanudavant66@gmail.com>
2024-06-13 14:13:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const  app = express()
+const cors = require("cors")
+const mongoose =require("mongoose")
+
+app.use(cors())
+app.use(express.json())
+
+const schemaData =mongoose.Schema({
+    name: String,
+    std: Number,
+    email: String,
+    contactno: Number,
+    address: String,
+},{
+    timestamps: true
+}
+)
+
+const userModel =mongoose.model("user",schemaData)
+
+//read 
+app.get("/",async (req,res)=>{
+    const data =await userModel.find({})
+    // res.json({success: true,data:data})
+
+    res.json({success: true,data:data})
+
+    //settimeout(function,3000)
+})
+
+//create data //save data in mongodb
+app.post("/create",async(req,res)=>{
+    console.log(req.body)
+    const data =new userModel(req.body)
+    await data.save()
+    res.send({success: true, message:"data saved successfully",data : data})
+}
+)
+
+app.put("/update",async(req,res)=>{
+    console.log(req.body)
+    const {_id,...rest} =req.body
+    console.log(rest)
+    const data = await userModel.updateOne({_id : _id},rest)
+    res.send({success: true, message:"data updated successfully", data:data})
+}
+)
+
+app.delete("/delete/:id",async(req,res)=>{
+    const id =req.params.id
+    console.log(id)
+    const data = await userModel.deleteOne({_id : id})
+    res.send({success: true, message:"data deleted successfully", data:data})
+}
+)
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentdata")
+.then(()=>{
+    console.log("connected to DB")
+    app.listen(8000,()=>console.log("server is running"))
+}
+)
+.catch((err)=>console.log(err))
+
+
